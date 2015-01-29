1882a1b57b39ba4709f549383cd72063df065503
Landey Gu <Landey@dhcp89.merunorth98.iit.edu>
2015-01-29 16:38:45 -0600

    ant file

---
+++ Documents/workspace/445Assignment1/src/Ant.java
@@ -0,0 +1,11 @@
+
+public class Ant extends Creature{
+		
+		public Ant(String Name1){
+			super(Name1);
+		}
+		
+		public void move(){
+			System.out.print(name +" Ant " + " is crawling around\n");
+		}
+}
